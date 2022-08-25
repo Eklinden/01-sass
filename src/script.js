79f5b1d2143f68cfbedfc3ec6c252073b3cd45b8
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-08-25 13:31:55 +0200

    fixade allt

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-console.log("damn boi");
+const dialogElem = document.getElementById("dialog-content");
+const buttonShowElem = document.getElementById("btn-show");
+
+document.getElementById("btn-cta").addEventListener("click", () => {
+  dialogElem.classList.toggle("dissapear");
+  buttonShowElem.classList.toggle("dissapear");
+});
+
+buttonShowElem.addEventListener("click", () => {
+  dialogElem.classList.toggle("dissapear");
+  buttonShowElem.classList.toggle("dissapear");
+});
